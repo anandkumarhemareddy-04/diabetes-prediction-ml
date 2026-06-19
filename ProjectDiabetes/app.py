cd5c2462b4fd90ff67682b2ee2678a1927b301f6
Anand Kumar <anandkumarhemareddy@gmail.com>
2026-06-19 19:30:18 +0530

    Dockerized diabetes prediction app

--- ProjectDiabetes/app.py
+++ ProjectDiabetes/app.py
@@ -40,5 +40,5 @@ def predict():
     except Exception as e:
         return render_template('index.html', prediction_text=f'Error: {str(e)}', form_data=None)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
